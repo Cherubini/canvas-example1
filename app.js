7644007b7c00eaf02e8cb1c32fe2785c52b9f77a
Cherubini <127298157+Cherubini@users.noreply.github.com>
2023-04-19 21:57:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const myCanvas = document.getElementById('my-canvas');
 
 const ctx = myCanvas.getContext('2d');
 
-
-for (let i = 0; i < 100; i++) {
+ for (let i = 0; i < 100; i++) {
     
     const originX = Math.random() * 600;
     const originy = Math.random() * 600;
     const width = Math.random() * 100;
     const height = Math.random() * 100;
 
-    ctx.strokeStyle = 'white'
+    const red = Math.random() * 355;
+    const green =Math.random() * 355;
+    const blue =Math.random() * 355;
 
+
+    ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+    ctx.fillRect(originX,originy,width,height);
+    
+    ctx.strokeStyle = `rgb(${red}, ${green}, ${blue})`;
+    ctx.lineWidth   = 5;
     ctx.strokeRect(originX,originy,width,height);
-}
+ }
